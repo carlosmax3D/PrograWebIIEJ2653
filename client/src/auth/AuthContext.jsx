2b95fd4d477c91816c9aa520cf44b67ef18c2780
Your Name <you@example.com>
2026-05-07 20:44:37 -0600

    Adjustent in authcontext

--- client/src/auth/AuthContext.jsx
+++ client/src/auth/AuthContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react'; 
+import React, { createContext, useContext, useState, useEffect } from 'react'; 
 
 const AuthContext = createContext(); 
 export function useAuth() { 
@@ -6,7 +6,20 @@ export function useAuth() {
 } 
 
 export function AuthProvider({ children }) { 
-const [user, setUser] = useState(null); 
+const [user, setUser] = useState(()=>{
+    const cacheUser = localStorage.getItem('PrograWebIIEJ2653_user');
+    return cacheUser ? JSON.parse(cacheUser)
+    : null
+});
+
+useEffect(()=>{
+    if (user){
+        localStorage.setItem('PrograWebIIEJ2653_user', JSON.stringify(user))
+    }else{
+        localStorage.removeItem('PrograWebIIEJ2653_user')
+    }
+}, [user])
+
 const login = (userData) => { 
 // Lógica de inicio de sesión aquí 
 setUser(userData); 
